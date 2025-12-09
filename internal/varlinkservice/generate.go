8f45e30ae279728a2ad2454341aa4ac3faa6834c
mhorky <mhorky@redhat.com>
2025-12-09 14:20:32 +0100

    Remove internal varlinkservice package
    It cannot be used to provide introspection for Registry due to circular
    dependencies. Since it is internal, it cannot be used by other projects
    either.
    
    The project now includes `example/` directory, which should act as an
    adequate replacement of sample implementation.

--- internal/varlinkservice/generate.go
+++
@@ -1,9 +0,0 @@
-//go:build generate
-
-package varlinkservice
-
-import (
-	_ "github.com/emersion/go-varlink/cmd/varlinkgen"
-)
-
-//go:generate go run github.com/emersion/go-varlink/cmd/varlinkgen -i org.varlink.service.varlink
